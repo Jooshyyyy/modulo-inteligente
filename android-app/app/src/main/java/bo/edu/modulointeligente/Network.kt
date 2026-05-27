76d515f9c935952a4575163f373880722b862c4c
Ervin Ledezma <eledezjob787@gmail.com>
2026-05-26 23:22:37 -0400

    revision de cambios por joshy

--- android-app/app/src/main/java/bo/edu/modulointeligente/Network.kt
+++ android-app/app/src/main/java/bo/edu/modulointeligente/Network.kt
@@ -82,7 +82,7 @@ interface ApiService {
 }
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.13.229:3000/"
+    private const val BASE_URL = "http://192.168.13.27:3000/"
      
     var authToken: String? = null
 
